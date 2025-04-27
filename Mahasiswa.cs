cb2a566235df4a6ce99957a9f8e59783c2874a53
Naufal Fahreza <naufalfahreza328@gmail.com>
2025-04-27 18:55:32 +0700

    menambahkan API mahasiswa

--- Mahasiswa.cs
+++ Mahasiswa.cs
@@ -2,6 +2,28 @@
 {
     public class Mahasiswa
     {
+        private string nama { get; set; }
+        private string nim { get; set; }
 
+        public Mahasiswa() { }
+
+        public Mahasiswa(string nama, string nim)
+        {
+            this.nama = nama;
+            this.nim = nim;
+        }
+
+        public string Nama
+        {
+            get { return nama; }
+            set { nama = value; }
+        }
+
+        public string NIM
+        {
+            get { return nim; }
+            set { nim = value; }
+        }
     }
 }
+
---
+++ MahasiswaController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace tpmodul10_103022330105
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class MahasiswaController : ControllerBase
+    {
+        private static List<Mahasiswa> daftarMahasiswa = new List<Mahasiswa>
+        {
+            new Mahasiswa{Nama = "Naufal Fahreza", NIM = "103022330105"},
+            new Mahasiswa{Nama = "Raka Valrizqy Akhdansyah", NIM = "103022330096"},
+            new Mahasiswa{Nama = "Muhammad Faiz Adya", NIM = "103022300048"},
+            new Mahasiswa{Nama = "Muhammad Rakan Yusra", NIM = "103022330044"},
+            new Mahasiswa{Nama = "Duhan Hasanain", NIM = "103022330080"}
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Mahasiswa>> GetAllMahasiswa()
+        {
+            return daftarMahasiswa;
+        }
+
+        [HttpGet("{index}")]
+        public ActionResult<Mahasiswa> GetMahasiswaIdx(int index)
+        {
+            if (index < 0 || index >= daftarMahasiswa.Count)
+            {
+                return NotFound();
+            }
+            return daftarMahasiswa[index];
+        }
+
+        [HttpPost]
+        public ActionResult AddMahasiswa([FromBody] Mahasiswa mhs)
+        {
+            daftarMahasiswa.Add(mhs);
+            return Ok();
+        }
+        [HttpDelete("{index}")]
+        public ActionResult DeleteMahasiswa(int index)
+        {
+            if (index < 0 || index >= daftarMahasiswa.Count)
+            {
+                return NotFound();
+            }
+            daftarMahasiswa.RemoveAt(index);
+            return Ok();
+        }
+
+    }
+}
